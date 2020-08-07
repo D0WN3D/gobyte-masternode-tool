bfc0854b1c5ef901a10bde02ba2bde9986af90e6
Antonio Moratti <antoniomladin@gmail.com>
2020-08-07 03:48:14 +0300

    Initial Commit

---
+++ src/default_config.py
@@ -0,0 +1,54 @@
+
+# default "public" connections for RPC proxy
+gobyted_default_connections = [
+    {
+        "use_ssh_tunnel": False,
+        "host": "gmt1.gobytenation.org",
+        "port": "443",
+        "username": "gmtuser",
+        "password": "x2oader5ddjh604dr76c8hqbu1ovkgdxhu40eew4x9kkm3kz4l79fp7nopr0nya2qeq186bu9enrjvro4n54twcye0lm4dnevq6",
+        "use_ssl": True
+    },
+    {
+        "use_ssh_tunnel": False,
+        "host": "gmt2.gobytenation.org",
+        "port": "443",
+        "username": "gmtuser",
+        "password": "x2oader5ddjh604dr76c8hqbu1ovkgdxhu40eew4x9kkm3kz4l79fp7nopr0nya2qeq186bu9enrjvro4n54twcye0lm4dnevq6",
+        "use_ssl": True
+    },
+    {
+        "use_ssh_tunnel": False,
+        "host": "gmt3.gobytenation.org",
+        "port": "443",
+        "username": "gmtuser",
+        "password": "x2oader5ddjh604dr76c8hqbu1ovkgdxhu40eew4x9kkm3kz4l79fp7nopr0nya2qeq186bu9enrjvro4n54twcye0lm4dnevq6",
+        "use_ssl": True
+    },
+    {
+        "use_ssh_tunnel": False,
+        "host": "ec2-13-49-148-169.eu-north-1.compute.amazonaws.com",
+        "port": "8080",
+        "username": "gobyte",
+        "password": "gobyte",
+        "use_ssl": True
+    },
+    {
+        "use_ssh_tunnel": False,
+        "host": "testnet1.gobytenation.org",
+        "port": "8443",
+        "username": "gmtuser",
+        "password": "x2oader5ddjh604dr76c8hqbu1ovkgdxhu40eew4x9kkm3kz4l79fp7nopr0nya2qeq186bu9enrjvro4n54twcye0lm4dnevq6",
+        "use_ssl": True,
+        "testnet": True
+    },
+    {
+        "use_ssh_tunnel": False,
+        "host": "testnet2.gobytenation.org",
+        "port": "8443",
+        "username": "gmtuser",
+        "password": "x2oader5ddjh604dr76c8hqbu1ovkgdxhu40eew4x9kkm3kz4l79fp7nopr0nya2qeq186bu9enrjvro4n54twcye0lm4dnevq6",
+        "use_ssl": True,
+        "testnet": True
+    }
+]
